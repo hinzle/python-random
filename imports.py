20043eaf25104d081857b413efde1a7f0872c193
hinzle <bosque4d@hotmail.com>
2022-03-01 15:52:31 -0500

    2022 Mar 01 : 15:52 EST : updated

--- imports.py
+++ imports.py
@@ -1,13 +1,13 @@
 # local_host
 from env import get_db_url
 import os
-import acquire
+
 
 # python data science library's
 from scipy import stats
 import numpy as np
 import pandas as pd
-import sklearn
+import sklearn as sk
 
 # visualizations
 from pydataset import data
