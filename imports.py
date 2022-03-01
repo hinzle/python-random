12c1a25752e3a23bfb45e4281e94604e63393202
hinzle <bosque4d@hotmail.com>
2022-03-01 09:55:03 -0500

    2022 Mar 01 : 09:54 EST : updated

--- imports.py
+++ imports.py
@@ -1,10 +1,18 @@
+# local_host
+from env import get_db_url
 import os
+import acquire
+
+# python data science library's
+from scipy import stats
 import numpy as np
 import pandas as pd
+import sklearn
+
+# visualizations
+from pydataset import data
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy import stats
-from pydataset import data
-from env import get_db_url
 
+# state properties
 np.random.seed(123)
